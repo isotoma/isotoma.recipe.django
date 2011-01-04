7dcb19122948a53c4c13df4cae00cc916a9ad2b2
Tom Wardill <tom@howrandom.net>
2011-01-04 15:25:05 +0000

    Throwing out the old stuff

--- isotoma/recipe/django/admin.py
+++
@@ -1,4 +0,0 @@
-from django.core import management
-
-def main():
-    management.execute_from_command_line()
--- isotoma/recipe/django/testing/django/core/management.py
+++
@@ -1,5 +0,0 @@
-def execute_manager(*args, **kwargs):
-    pass
-
-def execute_from_command_line(*args, **kwargs):
-    pass
