8d25190cc5b70bebe439a6a16a199855c50f0cd0
Tom Wardill <tom@howrandom.net>
2011-01-05 16:37:38 +0000

    Can create a project and apps

---
+++ isotoma/recipe/django/recipe.py
@@ -0,0 +1,131 @@
+import logging
+import os
+import sys
+
+import zc.recipe.egg
+from zc.buildout import easy_install
+
+class Recipe(object):
+    """ A buildout recipe to install django, and configure a project """
+    
+    def __init__(self, buildout, name, options):
+        """ Set up the options and paths that we will use later in the recipe """
+        
+        # set up some bits for the buildout to user
+        self.log = logging.getLogger(name)
+        self.egg = zc.recipe.egg.Egg(buildout, options['recipe'], options)
+        
+        # set the options that we've been passed so we can get them when we install
+        self.buildout = buildout
+        self.name = name
+        self.options = options
+        
+        # set the paths that we'll need
+        self.options['bin-directory'] = buildout['buildout']['bin-directory'] # where the control scripts are going to live
+        
+    def install(self):
+        """ Create and set up the project """
+        
+        # work out where we're creating the project
+        base_dir = self.buildout['buildout']['directory'] # the base directory for the installed files
+        src_dir = os.path.join(base_dir, 'src')
+        project_dir = os.path.join('src', self.options['project'])
+        
+        # this takes the src_dir as it creates the project folder for us
+        project = self.create_project(src_dir, self.options['project'])
+            
+        # now we have a project, we need to create any apps that we've been given
+        apps = self.options.get('apps', '').split()
+        for app in apps:
+            self.create_app(project_dir, app)
+            
+        # install the control scripts for django
+        self.install_scripts()
+        
+        return self.options.created()
+    
+    def install_scripts(self):
+        """ Install the control scripts that we need """
+        
+        # get the paths that we need
+        path = self.buildout["buildout"]["bin-directory"]
+        egg_paths = [
+            self.buildout["buildout"]["develop-eggs-directory"],
+            self.buildout["buildout"]["eggs-directory"],
+            ]
+        
+        # use the working set to correctly create the scripts with the correct python path
+        ws = easy_install.working_set(["isotoma.recipe.django"], sys.executable, egg_paths)
+        easy_install.scripts([('django-admin', "django.core.management", "execute_from_command_line")], ws, sys.executable, path)
+        
+        # add the created scripts to the buildout installed stuff, so they get removed correctly
+        self.options.created(os.path.join(path, 'django-admin'))
+        
+    def create_project(self, src_dir, project_name):
+        """ Create the django project in the correct directory 
+
+        Arguments:
+        src_dir - Directory to create the project in (it will create a containing directory for the project)
+        project_name - Name of the project to create
+        app_list - List of the apps to create in the project
+        
+        Returns a path to the created project directory
+        """
+        
+        # check if the project already exists
+        project_dir = os.path.join(src_dir, project_name)
+        if os.path.exists(project_dir):
+            self.log.info("Project %s already exists" % project_name)
+            return project_dir
+        
+        self.log.info("Creating project: %s in %s" % (project_name, src_dir))
+        
+        # at this point, the project directory should not exist, so create it
+        if not os.path.exists(src_dir): os.makedirs(src_dir)
+        
+        # now we need to be in the project directory, as the project creation tools cannot accept a dir, they use cwd
+        existing_path = os.getcwd()
+        os.chdir(src_dir)
+        
+        # we're now in the project directory, time to create it.
+        from django.core.management.commands import startproject
+        project = startproject.Command()
+        project.handle_label(project_name)
+        
+        # change back to the old directory, so we don't screw anyone else up
+        os.chdir(existing_path)
+        
+        self.log.info("Done creating project")
+        
+        return src_dir
+    
+    def create_app(self, project_dir, app_name):
+        """ Create an app in the project
+        
+        Arguments:
+        project_dir - the directory of the project
+        app_name - name of the app that is created
+        
+        Returns a path to the created app directory
+        """
+        # we need this to return it
+        app_dir = os.path.join(project_dir, app_name)
+        
+        # if the app already exists, don't create a new one
+        if os.path.exists(app_dir):
+            self.log.info("App %s already exists at %s" % (app_name, app_dir))
+            return app_dir
+        
+        # if we get here, we need a new app
+        self.log.info("Creating app: %s in directory %s" % (app_name, project_dir))
+        
+        # now create the app
+        from django.core.management.commands import startapp
+        app = startapp.Command()
+        app.handle_label(app_name, project_dir)
+        
+        self.log.info("Done creating app: %s" % app_name)
+        
+        return app_dir
+    
+        
