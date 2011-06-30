89204467df16dc6c4658992fce41b4e31b2c1a07
Tom Wardill <tom@howrandom.net>
2011-06-30 09:15:28 +0100

    Can pass extra paths through to the management scripts

--- isotoma/recipe/django/recipe.py
+++ isotoma/recipe/django/recipe.py
@@ -34,6 +34,12 @@ class Recipe(object):
         # template environment
         self.template_environment = Environment(loader=PackageLoader('isotoma.recipe.django', 'templates'))
         
+        # get the extra paths that we might need
+        if self.options.has_key('extra-paths'):
+            self.extra_paths = [path.strip() for path in self.options['extra-paths'].split('\n')]
+        else:
+            self.extra_paths = []
+        
     def install(self):
         """ Create and set up the project """
         
@@ -66,13 +72,13 @@ class Recipe(object):
         
         # use the working set to correctly create the scripts with the correct python path
         ws = easy_install.working_set(eggs_to_install, self.buildout['buildout']['executable'] ,egg_paths)
-        easy_install.scripts([('django-admin', "django.core.management", "execute_from_command_line")], ws, sys.executable, path)
+        easy_install.scripts([('django-admin', "django.core.management", "execute_from_command_line")], ws, sys.executable, path, extra_paths = self.extra_paths)
         
         # this is a bit nasty
         # we need to add the project to the working set so we can import from it
         # so we're adding it's directory as an extra_path, as egg installing it doesn't seem to be much success
         # install the project script ('manage.py')
-        easy_install.scripts([(self.options['control-script'], 'django.core.management', 'execute_manager')], ws, sys.executable, path, arguments='settings', initialization="import %s.%s as settings" % (self.options['project'], self.options['settings']))
+        easy_install.scripts([(self.options['control-script'], 'django.core.management', 'execute_manager')], ws, sys.executable, path, arguments='settings', initialization="import %s.%s as settings" % (self.options['project'], self.options['settings']), extra_paths = self.extra_paths)
         
         # install the wsgi script if required
         if self.options['wsgi'].lower() == 'true':
@@ -81,7 +87,7 @@ class Recipe(object):
             # we need to reset the template, as we need a custom script, rather than the standard buildout one
             _script_template = zc.buildout.easy_install.script_template # store the old one
             zc.buildout.easy_install.script_template = zc.buildout.easy_install.script_header + open(os.path.join(os.path.dirname(__file__), 'templates/wsgi.tmpl')).read() # set our new template
-            easy_install.scripts([(wsgi_name, 'isotoma.recipe.django.wsgi', 'main')], ws, sys.executable, path, arguments='settings', initialization="import %s.%s as settings" % (self.options['project'], self.options['settings']), extra_paths = [os.path.realpath(project_dir)])
+            easy_install.scripts([(wsgi_name, 'isotoma.recipe.django.wsgi', 'main')], ws, sys.executable, path, arguments='settings', initialization="import %s.%s as settings" % (self.options['project'], self.options['settings']), extra_paths = [os.path.realpath(project_dir)] + self.extra_paths)
             zc.buildout.easy_install.script_template = _script_template
         
         # add the created scripts to the buildout installed stuff, so they get removed correctly
